f48f80e9310d1dd9346119a9eacbfcb0972194e3
Valniria Bandeira <valniria.bandeira@to-brasil.com>
2020-09-25 10:48:01 -0300

    Configuraões de StartUp e outras

--- InvilliaApi/DesafioInvillia/Startup.cs
+++ InvilliaApi/DesafioInvillia/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DesafioInvillia
 {
@@ -25,12 +26,41 @@ namespace DesafioInvillia
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                   name: "AllowOrigin",
+                   builder => {
+                       builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+                   });
+            });
+
             services.AddControllers();
+
+            services.AddHttpClient();
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Desafio Invillia - Sistema de Controle de Jogos",
+                        Version = "v1",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Valniria Bandeira",
+                            Url = new System.Uri("https://github.com/valniria")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,6 +75,11 @@ namespace DesafioInvillia
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Desafio Invillia - Sistema de Controle de Jogos");
             });
         }
     }
